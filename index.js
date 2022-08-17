0b56bb880a0455c9382402b9f5b7d2f527a24922
Avleen Dhillon <avleendhillon@gmail.com>
2022-08-17 11:03:02 -0700

    Added all of the questions in the const questions portion

--- index.js
+++ index.js
@@ -6,8 +6,48 @@ const questions = [
         type: 'input',
         name: "title",
         message: "What would you like to title your ReadMe?",
-        
-    }
+    },
+    {
+        type: 'input',
+        name: "description",
+        message: "How would you describe your work??",
+    },
+    {
+        type: 'list',
+        name: "license",
+        message: "What kind of license would you like to use?",
+        choices: ["MIT", "APACHE", "GPL"]
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: "What installations did you have to go through?",
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: "How do you use the application?",
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: "What are the contributions?",
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: "What test did you run in order to get this ourcome?",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your email?",
+    },
+    {
+        type: 'input',
+        name: 'gitHub',
+        message: "What is your GitHub username?",
+    },
 ];
 
 // TODO: Create a function to write README file
